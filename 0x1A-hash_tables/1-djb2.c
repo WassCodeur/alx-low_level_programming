1730dcd0f5f22d138d2496a47fdf6948e2a27eb6
WassCodeur <wachioubouraima56@gmail.com>
2023-01-14 12:36:50 +0000

    update

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * function hash_table_create
+ *
+ * @parameter: size of the hashtable
+ * @description: a function that creates a hash table
+ * returns: Returns a pointer to the newly created hash table
+ */
+
+ hash_table_t *hash_table_create(unsigned long int size){
+    hash_table_t* table = (hash_table_t*)malloc(sizeof(hash_table_t));
+ }
