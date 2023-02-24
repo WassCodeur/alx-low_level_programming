feeb30c0b03e91841b14bc0355251ddc141adb45
WassCodeur <wachioubouraima56@gmail.com>
2023-02-24 21:19:13 +0000

    update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,7 +3,7 @@
 
 
 def island_perimeter(grid):
-     """returns the perimeter of the island described in grid"""
+    """returns the perimeter of the island described in grid"""
     width = 0
 
     for i in range(len(grid)):
