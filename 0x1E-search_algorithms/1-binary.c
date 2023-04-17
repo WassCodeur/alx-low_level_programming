5a611fefd00b00c91ecba8050a9657d148218ebd
WassCodeur <wachioubouraima56@gmail.com>
2023-04-17 00:50:59 +0000

    search algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * Enter point : binary_search
+ *
+ * Description  : function that searches for a value in an array of integers using the binary search algorithm
+ * return
+ */
+int binary_search(int *array, size_t size, int value)    
+{}
