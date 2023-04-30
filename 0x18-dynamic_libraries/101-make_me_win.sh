610f09e8da40935e99f16c7883c1f70bf83686f7
WassCodeur <wachioubouraima56@gmail.com>
2023-04-30 16:23:42 +0000

    laste advance task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -shared  -o librandom.so random.o
-export LD_PRELOAD=$(pwd)/librandom.s ./gm 9 8 10 24 75 9
+gcc -shared -o librandom.so random.o
+export LD_PRELOAD=$(pwd)/librandom.so
+
