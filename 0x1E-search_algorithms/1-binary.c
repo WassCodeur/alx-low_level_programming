aa7b79ef9fb1765fc357ea468dac3ca53029203d
WassCodeur <wachioubouraima56@gmail.com>
2023-05-11 08:28:14 -0700

    fix betty style error for the tak 2

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,34 +1,45 @@
 #include <stdio.h>
 #include "search_algos.h"
 /**
- * function make a binary search of a given value
+ * binary_search - Enter point
+ * @size : the number of element is array
+ * @value : the value we want to search in the array
+ * @array : pointer to the first value
  *
- * paramater : array, size, value
  *
- * return value location else -1
+ * Return: where the value located else -1
  */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t i, d, left, right;
 
-size_t i, d,left, right;
-left = 0;
-right = size - 1;
-do {
-printf("Searching in array: ");
-for( i = left; i < right ; i++) {
-printf("%d, ",array[i]);
-}
-printf("%d\n",array[i]);
-d = (left + right)/2;
-if(array[d] > value) {
-right = d - 1;
-}else if(array[d] < value ) {
-left = d + 1;
-}
-else if(array[d] == value) {
-return d;
-}
-}while(left <= right);
-return -1;
+	if (array == NULL || size == 0)
+	{
+		return (-1);
+	}
+
+	left = 0;
+	right = size - 1;
+	do {
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d\n", array[i]);
+
+		d = (left + right) / 2;
+		if (array[d] > value)
+		{
+			right = d - 1;
+		} else if (array[d] < value)
+		{
+			left = d + 1;
+		} else if (array[d] == value)
+		{
+			return (d);
+		}
+	} while (left <= right);
+	return (-1);
 
 }
