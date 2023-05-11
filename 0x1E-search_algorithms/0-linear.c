bbd307db2e7c41024bdc2bb5677f40d52f6e1300
WassCodeur <wachioubouraima56@gmail.com>
2023-05-11 06:42:39 -0700

    fix betty style error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include "search_algos.h"
 /**
- * Enter point : lenear_search
- * Function that search a number
- * 
- * parameters : array, size, value
+ * linear_search - Enter point
+ * @array : a pointer to the first element to array
+ * @size  : the number of element in the array
+ * @value : the value to search in the array
  *
- * return : value lacation
+ *
+ * Return: the first index where value is located
  */
 int linear_search(int *array, size_t size, int value)
 {
 
 size_t i;
-for(i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 
 printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-if(array[i] == value )
+if (array[i] == value)
 {
-return i;
+return (i);
 }
 }
-return -1;
+return (-1);
 }
